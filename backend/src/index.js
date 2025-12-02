5c00e72bc58a5620d7e2a915e1473a7f1e52db07
akku.dev <161335075+iamakkudev@users.noreply.github.com>
2025-12-02 18:18:31 +0530

    path resolve

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,8 +12,7 @@ import { fileURLToPath } from 'url'
 
 dotenv.config()
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __dirname = path.resolve
 
 const app = express()
 const PORT = process.env.PORT || 8080
@@ -48,7 +47,7 @@ app.use("/roadmap", roadmapRoutes)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-  app.get("*", (req, res) => {
+  app.get("/*path", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
   })
 }
