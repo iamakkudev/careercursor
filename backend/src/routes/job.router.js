30e19afe1f6ab1785ffcf2e57bd1a9bed3dd0fc6
akku.dev <161335075+iamakkudev@users.noreply.github.com>
2025-12-06 12:06:35 +0530

    fix: secure the joblist route

--- backend/src/routes/job.router.js
+++ backend/src/routes/job.router.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import { addJoblist, govJobs, moreJob, passionJobs, privateJobs, qualifyJobs,} from '../controllers/job.controller.js';
 import { limiter } from '../middleware/expressRateLimit.js';
+import {isLoggedIn} from '../middleware/auth.middleware.js'
 
 const router = express.Router()
 
 
-router.get("/private",privateJobs)
-router.get("/gov", govJobs)
-router.post("/addlist",addJoblist)
-router.post("/passion",limiter(3),passionJobs)
-router.post("/qualify",limiter(3),qualifyJobs)
-router.get("/more",limiter(1),moreJob)
+router.get("/private",isLoggedIn,privateJobs)
+router.get("/gov",isLoggedIn, govJobs)
+router.post("/addlist",isLoggedIn,addJoblist)
+router.post("/passion",isLoggedIn,limiter(3),passionJobs)
+router.post("/qualify",isLoggedIn,limiter(3),qualifyJobs)
+router.get("/more",isLoggedIn,limiter(1),moreJob)
 
 
 export default router;
--- backend/src/routes/roadmap.router.js
+++ backend/src/routes/roadmap.router.js
@@ -6,12 +6,12 @@ import { isLoggedIn } from '../middleware/auth.middleware.js';
 const router = express.Router()
 
 
-router.post("/",limiter(2),roadmap)
+router.post("/",isLoggedIn,limiter(2),roadmap)
 //to extract roadmap from database to show the user
 router.get("/",isLoggedIn,getRoadmap)
 router.post("/add",isLoggedIn,limiter(1),addRoadmap)
 router.post("/download",isLoggedIn,limiter(1),createRoadmapPdf)
-router.get("/download/:file",downloadRoadmapPdf)
+router.get("/download/:file",isLoggedIn,downloadRoadmapPdf)
 
 
 export default router;
